399b417451db9ad018f7c539c6538c52d8122c8c
mr-creature51 <99794089+mr-creature51@users.noreply.github.com>
2023-06-17 18:06:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+import StripeData from "./Stripe/StripeData.js"
+
+function App() {
+  return (
+    
+    <div>
+  <StripeData />
+       </div>
+    
+    
+  );
+}
+
+export default App;
